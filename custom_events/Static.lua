4f096645efcad884b67195c4ade1099f20a2e135
CarpetBook <forrestlightspeed@gmail.com>
2021-12-16 19:08:34 -0600

    Removed empty playSound function, stability confirmed on hako build

--- custom_events/Static.lua
+++ custom_events/Static.lua
@@ -1,17 +1,12 @@
 function onCreate()
     makeAnimatedLuaSprite('Static', 'screenstatic', 0, 0); -- "234" = sprite X pos, "567" = sprite Y pos
     addAnimationByPrefix('Static', 'idle', 'screenSTATIC', 24, false); -- "24" = framerate of animation, "false" = does the animation loop
-    setProperty('Static.alpha', 0);
-   
     addLuaSprite('Static', true); -- false = add behind characters, true = add over characters
     setObjectCamera('Static', 'hud'); -- shows up in the camera center as he should
 end
 
 function onEvent(name, value1, value2)
-   
     if name == 'Static' then
-        setProperty('Static.alpha', 255);
         objectPlayAnimation('Static', 'idle'); -- play the graphic's animation
-        playSound('soundfile');
-           end
+    end
 end

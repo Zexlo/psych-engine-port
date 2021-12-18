1c2f1935f593744494c788f2b937679ab2d5b92b
CarpetBook <forrestlightspeed@gmail.com>
2021-12-17 23:01:55 -0600

    Remove comment

--- custom_events/Note-Spin.lua
+++ custom_events/Note-Spin.lua
@@ -2,13 +2,9 @@ noteSpinSpeed = 0.15;
 
 function onEvent(name, value1, value2)
     if name == 'Note-Spin' then
-        for i=0, 7, 1 do
-            noteTweenAngle(i..'b', i, -360, 0.001, 'linear');
-            noteTweenAngle(i, i, 0, noteSpinSpeed, 'expoOut');
+        for i=0, 7, 1 do -- for every note
+            noteTweenAngle(i..'b', i, -360, 0.001, 'linear'); -- set to -360 to spin clockwise
+            noteTweenAngle(i, i, 0, noteSpinSpeed, 'expoOut'); -- reset to 0 to spin!! expo out gives the ease effect
         end
     end
 end
-
--- function onTweenCompleted(tag)
---     noteTweenAngle(tag, tag, 0, 0.001, 'linear');
--- end

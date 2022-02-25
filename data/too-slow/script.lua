37fb481ca0c72632fec7202b2a351d0422d63ad9
Zexlo <callum111.hotmail.com@gmail.com>
2022-02-25 20:08:52 +0000

    initial launch of the pre-countdown image loading

--- data/too-slow/script.lua
+++ data/too-slow/script.lua
@@ -1,8 +1,18 @@
  local allowCountdown = false
+ local stops = 0
 function onStartCountdown()
 	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
 		startVideo('SonicEXE');
-		allowCountdown = true;
+		end
+if not allowCountdown then		
+makeLuaSprite('CircleFaker', 'CircleFaker', -200, 200);
+addLuaSprite('CircleFaker',true);
+
+if stops == 1 then
+	allowCountdown = true;
+	setProperty('CircleFaker.visible',false)
+end	
+stops  = stops + 1	
 		return Function_Stop;
 	end
 	return Function_Continue;
@@ -11,6 +21,6 @@ end
 function onCreate()
 precacheSound('staticsound');
 precacheImage('sonicJUMPSCARE');
-    precacheImage('screenstatic');
-    precacheSound('Jumpscare');
+precacheImage('screenstatic');
+precacheSound('Jumpscare');
     end

4fd398787cfbf6278bd096b6762210fac475c355
Zexlo <callum111.hotmail.com@gmail.com>
2022-02-25 21:29:46 +0000

    blanked out countdownshit with empty assets

--- data/too-slow/script.lua
+++ data/too-slow/script.lua
@@ -1,14 +1,18 @@
  local allowCountdown = false
  local stops = 0
 function onStartCountdown()
-	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
-		startVideo('SonicEXE');
-		end
+
+if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
+startVideo('SonicEXE');
+
+stops  = stops + 1
+end
+
 if not allowCountdown then		
 makeLuaSprite('CircleFaker', 'CircleFaker', -200, 200);
 addLuaSprite('CircleFaker',true);
 
-if stops == 1 then
+if stops == 2 then
 	allowCountdown = true;
 	setProperty('CircleFaker.visible',false)
 end	

378bc2bfbd7acc4c59c75d8838de676ef930897b
Zexlo <callum111.hotmail.com@gmail.com>
2022-02-25 20:16:29 +0000

    added menuDesat, fixed MajGF colouring and transforms frames alignment

--- data/too-slow/script.lua
+++ data/too-slow/script.lua
@@ -1,18 +1,8 @@
- local allowCountdown = false
- local stops = 0
+local allowCountdown = false
 function onStartCountdown()
 	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
-		startVideo('SonicEXE');
-		end
-if not allowCountdown then		
-makeLuaSprite('CircleFaker', 'CircleFaker', -200, 200);
-addLuaSprite('CircleFaker',true);
-
-if stops == 1 then
-	allowCountdown = true;
-	setProperty('CircleFaker.visible',false)
-end	
-stops  = stops + 1	
+		startVideo('Phase2');
+		allowCountdown = true;
 		return Function_Stop;
 	end
 	return Function_Continue;

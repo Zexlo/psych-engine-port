2f5b50827e8ccb3dd3ac27d1536d1230619a9d77
Zexlo <callum111.hotmail.com@gmail.com>
2022-02-25 20:22:58 +0000

    inital pre-countdown image loading/unloading

--- data/too-slow/script.lua
+++ data/too-slow/script.lua
@@ -1,8 +1,18 @@
-local allowCountdown = false
+ local allowCountdown = false
+ local stops = 0
 function onStartCountdown()
 	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
-		startVideo('Phase2');
-		allowCountdown = true;
+		startVideo('SonicEXE');
+		end
+if not allowCountdown then		
+makeLuaSprite('CircleFaker', 'CircleFaker', -200, 200);
+addLuaSprite('CircleFaker',true);
+
+if stops == 1 then
+	allowCountdown = true;
+	setProperty('CircleFaker.visible',false)
+end	
+stops  = stops + 1	
 		return Function_Stop;
 	end
 	return Function_Continue;

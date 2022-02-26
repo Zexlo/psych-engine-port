cd319242b4187a60a4efd8cb2c7dfd5f67e8884a
CarpetBook <forrestlightspeed@gmail.com>
2022-02-25 20:06:38 -0600

    Revert "Indentation"
    This reverts commit 222c9c4a3b78a15fcea676b064454fe9973869a1.

--- data/script.lua
+++ data/script.lua
@@ -1,6 +1,18 @@
+local allowCountdown = false
+function onStartCountdown()
+	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
+	
+		allowCountdown = true;
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
+
+
 function onCreate()
 	setPropertyFromClass('GameOverSubstate', 'characterName', 'Majinbf'); --Character json file for the death animation
 	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx'); --put in mods/sounds/
 	setPropertyFromClass('GameOverSubstate', 'loopSoundName', ''); --put in mods/music/
 	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd'); --put in mods/music/
-end
+
+	end

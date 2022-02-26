e0e3d75212648ab7f138097301be59863f698cd8
CarpetBook <forrestlightspeed@gmail.com>
2022-02-25 20:05:14 -0600

    Added title animation for most songs #11

--- data/you-can't-run/script.lua
+++ data/you-can't-run/script.lua
@@ -13,12 +13,43 @@ function onCreate()
     addCharacterToList('SonicPixel', 'dad');
     addCharacterToList('BOYFRIEND', 'bf');
     addCharacterToList('Pixel_gf', 'gf');
-     addCharacterToList('gf', 'gf');
-
+    addCharacterToList('gf', 'gf');
 
     precacheImage('Greenhill');
     precacheImage('screenstatic');
     precacheImage('screenstaticBG');
     precacheImage('sonicJUMPSCARE');
 
+    precacheImage('black');
+
+	precacheImage('StartScreens/CircleYouCantRun');
+	precacheImage('StartScreens/TextYouCantRun');
+
+	makeLuaSprite('black', 'black', 0, 0);
+	addLuaSprite('black', true);
+	makeLuaSprite('circle', 'StartScreens/CircleYouCantRun', 1280, 0);
+	addLuaSprite('circle', true);
+	makeLuaSprite('text', 'StartScreens/TextYouCantRun', -1280, 0);
+	addLuaSprite('text', true);
+
+	setObjectCamera('black', 'hud');
+	setObjectCamera('circle', 'hud');
+	setObjectCamera('text', 'hud');
+end
+
+function onSongStart()
+	runTimer('flyin', 0.8);
+	runTimer('fadeout', 3);
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'flyin' then
+		doTweenX('circlefly', 'circle', 0, 1, 'linear');
+		doTweenX('textfly', 'text', 0, 1, 'linear');
+	end
+	if tag == 'fadeout' then
+		doTweenAlpha('fadeblack', 'black', 0, 2, 'sineOut');
+		doTweenAlpha('fadecircle', 'circle', 0, 2, 'sineOut');
+		doTweenAlpha('fadetext', 'text', 0, 2, 'sineOut')
+	end
 end
